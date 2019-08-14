4e4b36f42a3f8be4b118d7451df5800a5f97b718
clair13 <theclairvoyant1313@yahoo.gr>
2019-08-14 21:02:40 +0300

    Implement users new form and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "You havw registered succesfully"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Please try again"
+      render :new
+    end
   end
 
   def show
   end
 
+  private
 
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
