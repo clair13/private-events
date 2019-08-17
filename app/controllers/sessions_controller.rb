13ef94e5c160d82088360c2c29d1365d2f8b4c4e
clair13 <theclairvoyant1313@yahoo.gr>
2019-08-17 18:49:21 +0300

    Implement login logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:name])
+    if @user && @user.authenticate(params[:session][:email])
+      log_in @user
+      flash[:success] = "Successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "Invalid email"
+      render :new
+    end
   end
 
   def destroy
+    logout if logged_in?
+    redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def currrent_user
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
