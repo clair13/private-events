035d4d793527e6b168f93568d3a7dec821a631d0
clair13 <theclairvoyant1313@yahoo.gr>
2019-08-19 21:00:55 +0300

    Implement association between User and Event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :hosted_events, class_name: 'Event', foreign_key: 'creator_id'
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 70 }
