384c684f1b10a10c0346d3d63e1f9d08ea4fb061
clair13 <theclairvoyant1313@yahoo.gr>
2019-08-21 20:43:10 +0300

    Create Attendance model and assocciations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :events, class_name: 'Event', foreign_key: 'creator_id'
 
+  has_many :attendances
+  has_many :attended_events, through: :attendances, source: :event
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 70 }
